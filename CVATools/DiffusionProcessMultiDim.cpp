8844e62a90e80fd72fe47eb002139a346ab97812
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-03-29 10:04:04 +0100

    NEW : Declaration of Multidimensional DiffusionProcess

---
+++ CVATools/DiffusionProcessMultiDim.cpp
@@ -0,0 +1,53 @@
+//
+//  DiffusionProcessMultiDim.cpp
+//  CVATools
+//
+//  Created by Alexandre HUMEAU on 28/03/13.
+//
+//
+
+#include "DiffusionProcessMultiDim.h"
+#include <assert.h>
+#include <ranlib.h>
+
+DiffusionProcessMultiDim::DiffusionProcessMultiDim(std::size_t iNDimensions, const Matrix & sCorrelationMatrix) : iNDimensions_(iNDimensions), sCorrelationMatrix_(sCorrelationMatrix)
+{
+    assert(iNDimensions_ == sCorrelationMatrix_.getrows());
+    assert(iNDimensions_ == sCorrelationMatrix_.getcols());
+}
+
+DiffusionProcessMultiDim::~DiffusionProcessMultiDim()
+{
+    sListofDiffusionProcess_.clear();
+    sCorrelationMatrix_.~Matrix();
+}
+
+//  MultiDimensional Expectation
+DVector DiffusionProcessMultiDim::MultiExpectation(double t0, DVector dx, double dt) const
+{
+    assert(dx.size() == iNDimensions_);
+    DVector dResult(iNDimensions_), dMultiDrift = MultiDrift(dt, dx);
+    for (std::size_t iDim = 0 ; iDim < iNDimensions_ ; ++iDim)
+    {
+        dResult[iDim] = dx[iDim] + dMultiDrift[iDim];
+    }
+    return dResult;
+}
+
+//  MultiDimensional Variance
+Matrix DiffusionProcessMultiDim::MultiVariance(double t0, DVector dx, double dt) const
+{
+    assert(dx.size() == iNDimensions_);
+    Matrix dResult(iNDimensions_, iNDimensions_);
+    DVector dMultiVol = MultiVol(dt, dx);
+    for (std::size_t iRow = 0 ; iRow < iNDimensions_ ; ++iRow)
+    {
+        double dSigmai = dMultiVol[iRow];
+        for (std::size_t iCol = 0 ; iCol < iNDimensions_ ; ++iCol)
+        {
+            double dSigmaj = dMultiVol[iCol];
+            dResult(iRow, iCol) = dSigmai * dSigmaj * sCorrelationMatrix_(iRow,iCol) * dt;
+        }
+    }
+    return dResult;
+}
---
+++ CVATools/DiffusionProcessMultiDim.h
@@ -0,0 +1,40 @@
+//
+//  DiffusionProcessMultiDim.h
+//  CVATools
+//
+//  Created by Alexandre HUMEAU on 28/03/13.
+//
+//
+
+#ifndef CVATools_DiffusionProcessMultiDim_h
+#define CVATools_DiffusionProcessMultiDim_h
+
+#include "DiffusionProcess.h"
+#include "Matrix.h"
+#include <list>
+#include "Type.h"
+
+class DiffusionProcessMultiDim
+{
+protected:
+    std::size_t iNDimensions_;
+    std::vector<DiffusionProcess> sListofDiffusionProcess_;
+    Matrix sCorrelationMatrix_;
+public:
+    DiffusionProcessMultiDim(std::size_t iNDimension, const Matrix & sCorrelationMatrix);
+    virtual ~DiffusionProcessMultiDim();
+    
+    //  MultiDimensional Drift drift
+    virtual DVector MultiDrift(double dt, DVector dx) const = 0;
+    
+    //  MultiDimensional volatility vector
+    virtual DVector MultiVol(double dt, DVector dx) const = 0;
+    
+    //  MultiDimensional Expectation
+    virtual DVector MultiExpectation(double t0, DVector dx, double dt) const;
+    
+    //  MultiDimensional Variance
+    virtual Matrix MultiVariance(double t0, DVector dx, double dt) const;
+};
+
+#endif

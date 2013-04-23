7de477d0ac9194f7e7170847debff677caf6c28f
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-04-23 15:15:36 +0200

    FIX: forget to divide by bandwidth in Gaussian Kernel

--- CVATools/GaussianKernel.cpp
+++ CVATools/GaussianKernel.cpp
@@ -15,5 +15,5 @@ GaussianKernel::GaussianKernel(double dh) : KernelFunction(dh)
 double GaussianKernel::k(double u) const
 {
     // I dropped the \frac{1}{\sqrt{2\pi}} because it will simplify itself when doing the ratio
-    return exp(-0.5 * u * u);
+    return exp(-0.5 * u * u / (dh_ * dh_));
 }

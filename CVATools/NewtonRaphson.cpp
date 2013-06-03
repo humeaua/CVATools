7e046466a67373a6686364bf3c35abe23e93799b
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-06-03 20:53:37 +0100

    DEV: Add Generic Class for Newton Raphson algorithm

---
+++ CVATools/NewtonRaphson.cpp
@@ -0,0 +1,51 @@
+//
+//  NewtonRaphson.cpp
+//  CVATools
+//
+//  Created by Alexandre HUMEAU on 03/06/13.
+//
+//
+
+#include "NewtonRaphson.h"
+#include "Require.h"
+#include <cmath>
+#include <stdexcept>
+
+NewtonRaphson1D::NewtonRaphson1D(std::size_t iNIterMax, double dTolerance, double dEpsValueDeriv) : iNIterMax_(iNIterMax), dTolerance_(dTolerance), dEpsValueDeriv_(dEpsValueDeriv)
+{
+    Utilities::require(dEpsValueDeriv_ > 0.0, "NewtonRaphson1D : Epsilon value for derivative should be positive");
+    Utilities::require(dTolerance_ > 0.0, "NewtonRaphson1D : Tolerance should be positive");
+}
+
+double NewtonRaphson1D::algo_root_finder(double dInitialGuess) const
+{
+    //  Newton Raphson method to find the yield that correspond to the price of the bond
+    double dx = dInitialGuess, dfdx;
+    std::size_t iNIter = 0;
+    
+    double df0 = f(dx);
+    
+    while (fabs(df0) > dTolerance_ && iNIter <= iNIterMax_)
+    {
+        dfdx = df(dx);
+        if (dfdx > dEpsValueDeriv_)
+        {
+            dx -= df0 / df(dx);
+        }
+        else
+        {
+            throw std::runtime_error("NewtonRaphson1D : Derivative is too small.");
+        }
+        df0 = f(dx);
+        
+        //  Update number of iterations
+        iNIter++;
+    }
+    
+    if (iNIter >= iNIterMax_)
+    {
+        throw std::runtime_error("NewtonRaphson1D : Newton Algorithm has not converged");
+    }
+    
+    return dx;
+}
---
+++ CVATools/NewtonRaphson.h
@@ -0,0 +1,29 @@
+//
+//  NewtonRaphson.h
+//  CVATools
+//
+//  Created by Alexandre HUMEAU on 03/06/13.
+//
+//
+
+#ifndef __CVATools__NewtonRaphson__
+#define __CVATools__NewtonRaphson__
+
+#include <iostream>
+
+class NewtonRaphson1D {
+protected:
+    std::size_t iNIterMax_;
+    double dTolerance_;
+    double dEpsValueDeriv_;
+    
+public:
+    NewtonRaphson1D(std::size_t iNIterMax, double dTolerance, double dEpsValueDeriv);
+    
+    virtual double f(double x) const = 0;
+    virtual double df(double x) const = 0;
+    
+    virtual double algo_root_finder(double dInitialGuess) const;
+};
+
+#endif /* defined(__CVATools__NewtonRaphson__) */

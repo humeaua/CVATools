824328103967066812d1d385f64677a96a7253f2
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-06-04 21:53:16 +0100

    Revert "DEV: put virtual methods in matrix class"
    This reverts commit cd7fae514edf776b9d1a70ce10688d8042a6af8d.

--- CVATools/Matrix.h
+++ CVATools/Matrix.h
@@ -19,7 +19,7 @@ public:
 	Matrix(int N, int M);
     Matrix(std::size_t N, std::size_t M);
 	Matrix(const Matrix& m);
-	virtual ~Matrix();
+	~Matrix();
     
     void Reallocate(int N, int M);
     
@@ -29,11 +29,11 @@ public:
     double operator ()(std::size_t i, std::size_t j);
 	double operator ()(std::size_t i, std::size_t j) const;
     
-    virtual void set(int i, int j, double value);
-    virtual void set(std::size_t i, std::size_t j, double value);
+    void set(int i, int j, double value);
+    void set(std::size_t i, std::size_t j, double value);
     
-	virtual int getrows() const;
-	virtual int getcols() const;
+	int getrows() const;
+	int getcols() const;
     
 	void print();
     

5d22466d50eaae389669bc262b580d75e720b2cb
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-07-21 19:59:44 +0100

    DEV: try to add ridge regression to see the impact

--- ThirdParty/R/TestLinearisationPayoff.R
+++ ThirdParty/R/TestLinearisationPayoff.R
@@ -1,17 +1,27 @@
 filename = "/Users/alexhum49/Documents/Workspace/CVA/CVATools/ThirdParty/R/Data/FinalSpot.csv"
 
-data = read.csv(filename,header=FALSE)
-payoff = rep(0,length(data[,1]))
-
-for (i in 1:length(data[,1]))
-{
-  payoff[i] = max(data[i,1] - 1, 0)
-}
+simul = read.csv(filename,header=FALSE)
 
 # plot data to see if the acquisition is correct
-plot(data[,1], payoff)
-
-res = lm(payoff~data[,1])
+plot(simul$V1, simul$V2)
+names(simul)[2] <- "payoff"
+names(simul)[1] <- "und"
+res = lm(simul$payoff~simul$und)
 
 summary(res)
 abline(res)
+
+library(MASS)
+help(lm.ridge)
+#data(payoff)
+# does not work yet
+resridge = lm.ridge(simul$payoff ~ .,simul)
+
+data
+
+# get inspire by this following code
+data(longley) # not the same as the S-PLUS dataset
+names(longley)[1] <- "y"
+lm.ridge(y ~ ., longley)
+plot(lm.ridge(y ~ ., longley, lambda = seq(0,0.1,0.001)))
+select(lm.ridge(y ~ ., longley, lambda = seq(0,0.1,0.0001)))

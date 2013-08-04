115cd24652b4c80c3e651680dadf610919f927fc
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-08-04 16:08:59 +0100

    DEV: put newton raphson in namespace maths

--- CVATools/NewtonRaphson.cpp
+++ CVATools/NewtonRaphson.cpp
@@ -10,41 +10,43 @@
 #include <cmath>
 #include "Require.h"
 
-NewtonRaphson1D::NewtonRaphson1D(std::size_t iNIterMax, double dTolerance, double dEpsValueDeriv) : iNIterMax_(iNIterMax), dTolerance_(dTolerance), dEpsValueDeriv_(dEpsValueDeriv)
+namespace Maths
 {
-    Utilities::requireException(dEpsValueDeriv > 0, "NewtonRaphson1D : Epsilon value for derivative should be positive");
-    Utilities::requireException(dTolerance > 0, "NewtonRaphson1D : Tolerance should be positive");
-}
-
-double NewtonRaphson1D::algo_root_finder(double dInitialGuess) const
-{
-    //  Newton Raphson method to find the yield that correspond to the price of the bond
-    double dx = dInitialGuess, dfdx;
-    std::size_t iNIter = 0;
-    
-    double df0 = f(dx);
-    
-    while (fabs(df0) > dTolerance_ && iNIter <= iNIterMax_)
+    NewtonRaphson1D::NewtonRaphson1D(std::size_t iNIterMax, double dTolerance, double dEpsValueDeriv) : iNIterMax_(iNIterMax), dTolerance_(dTolerance), dEpsValueDeriv_(dEpsValueDeriv)
     {
-        dfdx = df(dx);
-        if (fabs(dfdx) > dEpsValueDeriv_)
-        {
-            dx -= df0 / df(dx);
-        }
-        else
-        {
-            throw Utilities::MyException("NewtonRaphson1D : Derivative is too small.");
-        }
-        df0 = f(dx);
+        Utilities::requireException(dEpsValueDeriv > 0, "NewtonRaphson1D : Epsilon value for derivative should be positive");
+        Utilities::requireException(dTolerance > 0, "NewtonRaphson1D : Tolerance should be positive");
+    }
+    
+    double NewtonRaphson1D::algo_root_finder(double dInitialGuess) const
+    {
+        //  Newton Raphson method to find the yield that correspond to the price of the bond
+        double dx = dInitialGuess, dfdx;
+        std::size_t iNIter = 0;
         
-        //  Update number of iterations
-        iNIter++;
+        double df0 = f(dx);
+        
+        while (fabs(df0) > dTolerance_ && iNIter <= iNIterMax_)
+        {
+            dfdx = df(dx);
+            if (fabs(dfdx) > dEpsValueDeriv_)
+            {
+                dx -= df0 / df(dx);
+            }
+            else
+            {
+                throw Utilities::MyException("NewtonRaphson1D : Derivative is too small.");
+            }
+            df0 = f(dx);
+            
+            //  Update number of iterations
+            iNIter++;
+        }
+        
+        if (iNIter >= iNIterMax_)
+        {
+            throw Utilities::MyException("NewtonRaphson1D : Newton Algorithm has not converged");
+        }
+        return dx;
     }
-    
-    if (iNIter >= iNIterMax_)
-    {
-        throw Utilities::MyException("NewtonRaphson1D : Newton Algorithm has not converged");
-    }
-    
-    return dx;
 }
--- CVATools/NewtonRaphson.h
+++ CVATools/NewtonRaphson.h
@@ -11,19 +11,23 @@
 
 #include <iostream>
 
-class NewtonRaphson1D {
-protected:
-    std::size_t iNIterMax_;
-    double dTolerance_;
-    double dEpsValueDeriv_;
-    
-public:
-    NewtonRaphson1D(std::size_t iNIterMax = 100, double dTolerance = 1e-06, double dEpsValueDeriv = 1e-06);
-    
-    virtual double f(double x) const = 0;
-    virtual double df(double x) const = 0;
-    
-    virtual double algo_root_finder(double dInitialGuess) const;
-};
+namespace Maths
+{
+    class NewtonRaphson1D
+    {
+    protected:
+        std::size_t iNIterMax_;
+        double dTolerance_;
+        double dEpsValueDeriv_;
+        
+    public:
+        NewtonRaphson1D(std::size_t iNIterMax = 100, double dTolerance = 1e-06, double dEpsValueDeriv = 1e-06);
+        
+        virtual double f(double x) const = 0;
+        virtual double df(double x) const = 0;
+        
+        virtual double algo_root_finder(double dInitialGuess) const;
+    };
+}
 
 #endif /* defined(__CVATools__NewtonRaphson__) */

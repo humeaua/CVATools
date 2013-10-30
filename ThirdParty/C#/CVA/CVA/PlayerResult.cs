9c8c896ece3d2aac1729269efad5259e124b49ba
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-10-30 21:44:44 +0000

    DEV: Add player Result

--- ThirdParty/C#/CVA/CVA/PlayerResult.cs
+++ ThirdParty/C#/CVA/CVA/PlayerResult.cs
@@ -5,10 +5,24 @@ namespace CVA
 	public class PlayerResult
 	{
 		private string cTournamentName;
+		private DateTime sTournamentDate;
+		private double dPoint;
 
-		public PlayerResult (ref string cTournamentName0)
+		public PlayerResult (ref string cTournamentName0, ref DateTime sTournamentDate0, ref double dPoint0)
 		{
 			cTournamentName = cTournamentName0;
+			sTournamentDate = sTournamentDate0;
+			dPoint = dPoint0;
+		}
+
+		public static bool operator < (PlayerResult sLeft, PlayerResult sRight)
+		{
+			return sLeft.sTournamentDate < sRight.sTournamentDate;
+		}
+
+		public static bool operator > (PlayerResult sLeft, PlayerResult sRight)
+		{
+			return sLeft.sTournamentDate > sRight.sTournamentDate;
 		}
 	}
 }

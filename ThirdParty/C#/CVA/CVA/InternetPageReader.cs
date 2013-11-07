8c48c36afebcb17c47e1047b01271f8661566dfb
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-11-07 20:37:36 +0000

    DEV: Add example to read csv file

--- ThirdParty/C#/CVA/CVA/InternetPageReader.cs
+++ ThirdParty/C#/CVA/CVA/InternetPageReader.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Net; // for Webclient
+using System.Text; // For Encoding
 
 namespace CVA
 {
@@ -9,10 +10,10 @@ namespace CVA
 
 		public void ToTextFile(ref string cWebsite, ref string cTextFile)
 		{
-			System.Net.WebClient wc = new System.Net.WebClient ();
+			WebClient wc = new System.Net.WebClient ();
 			byte[] raw = wc.DownloadData(cWebsite);
 
-			string webData = System.Text.Encoding.UTF8.GetString(raw);
+			string webData = Encoding.UTF8.GetString(raw);
 
 			//	Print in text file 
 			System.IO.File.WriteAllText(cTextFile, webData);
--- ThirdParty/C#/CVA/CVA/Program.cs
+++ ThirdParty/C#/CVA/CVA/Program.cs
@@ -1,4 +1,6 @@
 using System; // for Console
+using System.IO; // for File
+using System.Linq; // for Select
 using NameParser; // for HTML Parser
 
 namespace CVA
@@ -12,6 +14,7 @@ namespace CVA
 			Console.WriteLine ("1- Get files from Internet");
 			Console.WriteLine ("2- Parse player files");
 			Console.WriteLine ("3- Test HTML Parser");
+			Console.WriteLine ("4- CSV File Reader");
 			cChoice = Console.ReadLine ();
 		}
 
@@ -78,30 +81,45 @@ namespace CVA
 					Console.WriteLine ("Elapsed time to get all files : " + (DateTime.Now - ss));
 					
 					bIsFinished = IsFinish ();
-				} 
-				else if (cChoice == "2")
-			 	{
+				} else if (cChoice == "2") {
 					// do something
 					Console.WriteLine ("Not yet implemented");
 					bIsFinished = true;
-				} 
-				else if (cChoice == "3") 
-				{
+				} else if (cChoice == "3") {
 					Console.WriteLine ("Test of HTML Parser");
 					string cLine = "<b><i>blabla</i>  <u> blibli </u></b> bloblo";
 					TextStyle sDefaultStyle = new TextStyle ();
 					TextStyle.TextStyleCollection tsc = HTMLParser.ParseHTML (cLine, sDefaultStyle);
 
-					Console.WriteLine(tsc.ToString());
+					Console.WriteLine (tsc.ToString ());
 					TextStyle[] sTextStyleArray = new TextStyle[16];
-					tsc.CopyTo(sTextStyleArray);
-					for (int i = 0; i < sTextStyleArray.Length ; i ++) {
+					tsc.CopyTo (sTextStyleArray);
+					for (int i = 0; i < sTextStyleArray.Length; i ++) {
 						if (sTextStyleArray [i] != null) {
 							Console.WriteLine ("i = " + i + " : ");
-							Console.WriteLine(sTextStyleArray [i].ToString ());
+							Console.WriteLine (sTextStyleArray [i].ToString ());
 						} else {
 							Console.WriteLine ("i = " + i + " : null");
 						}
+					}
+
+					bIsFinished = true;
+				} 
+				else if (cChoice == "4") 
+				{
+					string cFileName = "/Users/alexhum49/Documents/Workspace/CVA/CVATools/Input/Golf/Players/Adam_Scott.csv";
+					var lines = File.ReadAllLines(cFileName).Select(a => a.Split(';'));
+					var csv = from line in lines
+						select (from piece in line
+						        select piece);
+
+					foreach (var element in csv) 
+					{
+						foreach (var element2 in element) 
+						{
+							Console.Write(element2 + " ; ");
+						}
+						Console.WriteLine("");
 					}
 
 					bIsFinished = true;

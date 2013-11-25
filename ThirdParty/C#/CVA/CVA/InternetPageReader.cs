1d064ee1012d2b9d9c29659abe8a908f2676c34c
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-11-25 16:24:37 +0100

    DEV: Add using directive

--- ThirdParty/C#/CVA/CVA/InternetPageReader.cs
+++ ThirdParty/C#/CVA/CVA/InternetPageReader.cs
@@ -1,5 +1,6 @@
 using System.Net; // for Webclient
 using System.Text; // For Encoding
+using System.IO; // for File
 
 namespace CVA
 {
@@ -10,13 +11,13 @@ namespace CVA
 
 		public void ToTextFile(ref string cWebsite, ref string cTextFile)
 		{
-			WebClient wc = new System.Net.WebClient ();
+			WebClient wc = new WebClient ();
 			byte[] raw = wc.DownloadData(cWebsite);
 
 			string webData = Encoding.UTF8.GetString(raw);
 
 			//	Print in text file 
-			System.IO.File.WriteAllText(cTextFile, webData);
+			File.WriteAllText(cTextFile, webData);
 		}
 	}
 }

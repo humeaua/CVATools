09f95c9e14a6caba4c9ce259c10bd2a92d9bedff
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2014-02-07 19:46:18 +0000

    DEV: Add Normalize function in SVI Parameter solver

--- CVATools/SVIParameterSolver.cpp
+++ CVATools/SVIParameterSolver.cpp
@@ -40,6 +40,8 @@ namespace Finance
                     double atmVol = volSmile(0.0);
                     dSigma_ = ((volSmile(firstStrike) - atmVol) / firstStrike - (volSmile(lastStrike) - atmVol) / lastStrike) / (firstStrike - lastStrike);
                     dM_ = 0.0; // need to check in this case
+                    
+                    REQUIREEXCEPTION(dSigma_ == 0, "Smile is flat");
                 }
             }
             else
@@ -47,5 +49,26 @@ namespace Finance
                 throw EXCEPTION("First strike must be negative and last strike must be positive");
             }
         }
+        
+        void SVIParameterSolver::Normalize(const std::vector<double> & LogStrikesInput,
+                                           const std::vector<double> & volatilitiesInput,
+                                           double T, // maturity
+                                           std::vector<double> & StrikesOutput,
+                                           std::vector<double> & VarianceOutput) const
+        {
+            REQUIREEXCEPTION(LogStrikesInput.size() == volatilitiesInput.size(), "Inputted Log-strikes and volatilities are not of the same size");
+            
+            StrikesOutput.resize(LogStrikesInput.size());
+            VarianceOutput.resize(LogStrikesInput.size());
+            
+            std::vector<double>::const_iterator volinput = volatilitiesInput.begin();
+            int i = 0;
+            for (std::vector<double>::const_iterator strike = LogStrikesInput.begin() ; strike != LogStrikesInput.end() ; ++strike, ++volinput, ++i)
+            {
+                VarianceOutput[i] = *volinput * *volinput * T;
+                StrikesOutput[i] = (*strike - dM_) / dSigma_;
+            }
+        }
+
     }
 }
--- CVATools/SVIParameterSolver.h
+++ CVATools/SVIParameterSolver.h
@@ -26,6 +26,13 @@ namespace Finance
             
             //  The parameter useParabola controls the fact that the parameters m and sigma are choosen by fitting a parabola, using the first quoted strike, the last quoted strike and the at-the-money volatility
             void SetFirstGuess(const VolSmile & volSmile, bool useParabola);
+            
+            //  The goal of the method is to normalize the volatilities and the strikes to apply the SVI resolution
+            void Normalize(const std::vector<double> & LogStrikesInput,
+                           const std::vector<double> & volatilitiesInput,
+                           double T, // maturity
+                           std::vector<double> & StrikesOutput,
+                           std::vector<double> & VarianceOutput) const;
         };
     }
 }

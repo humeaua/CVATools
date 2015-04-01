4e9b70febe21ce0761a26642abcffe5b858d5a78
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2015-04-01 11:13:35 +0100

    remove static variables

--- CVATools/FADBAD++/extra/ndf.h
+++ CVATools/FADBAD++/extra/ndf.h
@@ -54,7 +54,7 @@ inline double cnd(const double x)
 {
 	// CND(X)=0 for X<-37 and CND(X)=1 for X>37 when
 	// computing in double precision:
-	static const double CNDMAX=37;
+    const double CNDMAX=37;
 	
 	double cnorm;
 	double xabs=fabs(x);
@@ -103,30 +103,30 @@ inline double cnd(const double x)
 // this will cause inexact results!
 inline double icnd(const double p)
 {
-	static const double a[6] = 
+    const double a[6] = 
 	{
 		-3.969683028665376e+01,  2.209460984245205e+02,
 		-2.759285104469687e+02,  1.383577518672690e+02,
 		-3.066479806614716e+01,  2.506628277459239e+00
 	};
-	static const double b[5] = 
+    const double b[5] = 
 	{
 		-5.447609879822406e+01,  1.615858368580409e+02,
 		-1.556989798598866e+02,  6.680131188771972e+01,
 		-1.328068155288572e+01
 	};
-	static const double c[6] = 
+    const double c[6] = 
 	{
 		-7.784894002430293e-03, -3.223964580411365e-01,
 		-2.400758277161838e+00, -2.549732539343734e+00,
 		4.374664141464968e+00,  2.938163982698783e+00
 	};
-	static const double d[4] = 
+    const double d[4] = 
 	{
 		7.784695709041462e-03,  3.224671290700398e-01,
 		2.445134137142996e+00,  3.754408661907416e+00
 	};
-	static const double sqrt2pi=Op<double>::mySqrt(Op<double>::myTwo()*Op<double>::myPI());
+    const double sqrt2pi=Op<double>::mySqrt(Op<double>::myTwo()*Op<double>::myPI());
 	
 	double q, t, u;
 	

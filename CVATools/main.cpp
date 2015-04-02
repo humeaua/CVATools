28649fd7984cce29267f1d005c658b1bf8bc104a
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2015-04-02 10:15:29 +0100

    print output in file

--- CVATools/main.cpp
+++ CVATools/main.cpp
@@ -8,21 +8,45 @@
 
 #include "RegressionTestLauncher.h"
 #include "Exception.h"
+#include <fstream>
 
 //////////////////////////////////
 //  Forward Declaration
 //////////////////////////////////
 int _main();
+const std::string currentDateTime();
 
 ///////////////////////////////////
 //  Main functions
 ///////////////////////////////////
 
+// Get current date/time, format is YYYY-MM-DD.HH:mm:ss
+const std::string currentDateTime()
+{
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+    
+    return buf;
+}
+
 int _main()
 {
-    RegressionTestLauncher launcher(std::cout);
+    std::cout << "Launching tests" << std::endl;
+    const std::string filename = "Test_" + currentDateTime() + ".log";
+    std::fstream file(filename.c_str(), std::fstream::out);
+    RegressionTestLauncher launcher(file);
     bool bSucceeded = launcher.Launch();
     
+    std::cout << "Log captured in " << filename << std::endl;
+    if (!bSucceeded)
+    {
+        std::cout << "Tests finished with errors. see output file " << filename << std::endl;
+    }
     return (int)(!bSucceeded);
 }
 
@@ -30,19 +54,21 @@ int main()
 {
     try
     {
-        _main();
+        return _main();
     }
     catch (const Utilities::MyException & excep)
     {
         std::cout << "MyException caught : " << excep.what() << std::endl;
+        throw;
     }
     catch (const std::exception & excep)
     {
         std::cout << "std::exception caught : " << excep.what() << std::endl;
+        throw;
     }
     catch (...)
     {
         std::cout << "Unknown exception caught !" << std::endl;
+        throw;
     }
-    return 0; 
 }

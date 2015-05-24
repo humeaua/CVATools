3123eff9b3e24c192cb4703953aaff96969a8592
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2015-05-24 22:00:18 +0100

    new class config reader (will be based on config reader)

---
+++ CVATools/ConfigReader.cpp
@@ -0,0 +1,9 @@
+//
+//  ConfigReader.cpp
+//  CVATools
+//
+//  Created by Alexandre HUMEAU on 24/05/2015.
+//
+//
+
+#include "ConfigReader.h"
---
+++ CVATools/ConfigReader.h
@@ -0,0 +1,19 @@
+//
+//  ConfigReader.h
+//  CVATools
+//
+//  Created by Alexandre HUMEAU on 24/05/2015.
+//
+//
+
+#ifndef __CVATools__ConfigReader__
+#define __CVATools__ConfigReader__
+
+#include <stdio.h>
+
+class ConfigReader
+{
+    
+};
+
+#endif /* defined(__CVATools__ConfigReader__) */

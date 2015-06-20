83e6d9ba1ed214d0f386b936a6b0e9a77c92b59a
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2015-06-20 16:27:29 +0100

    change name of output file

--- CVATools/main.cpp
+++ CVATools/main.cpp
@@ -29,7 +29,7 @@ const std::string currentDateTime()
     tstruct = *localtime(&now);
     // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
     // for more information about date/time format
-    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%H-%M-%S", &tstruct);
     
     return buf;
 }
